da803f45e581717f8f5e1dfbc221bd59a87d29af
BD103 <59022059+BD103@users.noreply.github.com>
2022-08-31 07:56:25 -0400

    BREAKING(stdlib): clear stdlib again
    I'm not going to work on this until the parser is more developed.

--- crates/claw_stdlib/src/lib.rs
+++ crates/claw_stdlib/src/lib.rs
@@ -1,44 +1,8 @@
 //! Represents blocks included with the Scratch standard library.
 
-mod events;
-
-/// Represents an argument to a block.
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum ArgType {
-    /// A string.
-    String,
-
-    /// A number.
-    Number,
-
-    /// A boolean.
-    Boolean,
-
-    /// An enum.
-    Enum { kind: EnumKind },
-
-    /// Represents an unsupported argument that makes the block impossible to use.
-    ///
-    /// This is essentially a placeholder until I figure out if an argument type is worth having
-    /// its own enum, or another solution should be found.
-    Unsupported,
-}
-
 /// Represents different enums that can be accessed.
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum EnumKind {
     /// Accesses the local (sprite) and global (stage) variables.
     Variable,
 }
-
-pub trait Module: Clone + Copy {
-    /// Returns variant from given identifier.
-    fn from_ident(ident: &str) -> Option<Self>;
-
-    /// Returns the opcode of the current variant.
-    fn opcode(&self) -> &'static str;
-}
-
-pub trait BlockModule: Module {}
-
-pub trait EventModule: Module {}
