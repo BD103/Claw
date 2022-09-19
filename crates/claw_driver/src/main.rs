02ff8928d3eec4024b8986161ca6d9deeffa0620
BD103 <59022059+BD103@users.noreply.github.com>
2022-09-18 20:09:36 -0400

    refactor(driver): clippy fixes

--- crates/claw_driver/src/main.rs
+++ crates/claw_driver/src/main.rs
@@ -4,14 +4,14 @@ fn load_file(path: &Path) -> io::Result<String> {
     Ok(String::from_utf8_lossy(&fs::read(path)?).into_owned())
 }
 
-fn main() -> anyhow::Result<()> {
-    let script = if let Some(filename) = env::args().nth(1) {
-        let path = Path::new(&filename);
-        load_file(path).expect("Error loading file.")
-    } else {
+fn main() {
+    let script = env::args().nth(1).map_or_else(|| {
         let path = Path::new("./examples/stage1.claw");
         load_file(path).expect("Error loading file.")
-    };
+    }, |filename| {
+        let path = Path::new(&filename);
+        load_file(path).expect("Error loading file.")
+    });
 
     // Generate AST
     let ast = match claw::parse::parse(&script) {
@@ -33,6 +33,4 @@ fn main() -> anyhow::Result<()> {
         // Verify generated serde_json::Value
         // claw::verify::verify(project_json);
     }
-
-    Ok(())
 }
