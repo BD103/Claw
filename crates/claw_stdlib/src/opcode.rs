0286a7fdab2255537ccfbbad726376025593aa6d
BD103 <59022059+BD103@users.noreply.github.com>
2022-09-22 08:57:05 -0400

    feat(stdlib): better way of defining opcodes

--- crates/claw_stdlib/src/opcode.rs
+++ crates/claw_stdlib/src/opcode.rs
@@ -1,51 +1,80 @@
 use crate::Type::{self, *};
+use paste::paste;
+
+/// If given a value, returns [`Some(value)`](`Some`). Else, returns [`None`].
+///
+/// This exists so that return arguments are optional in [`create_opcodes`]. There's no way to create if-else statements with `macro_rules`, so this is a workaround.
+macro_rules! create_optional {
+    ($value:expr) => {
+        Some($value)
+    };
+    () => {
+        None
+    };
+}
 
 macro_rules! create_opcodes {
-    {
-        $($opname:ident => ($($oparg:expr),*): $opreturn:expr => $opcode:ident),*
-    } => {
-        // #[derive(Debug, Copy, Clone, PartialEq, Eq)]
-        pub enum OpCode {
-            $($opname),*
-        }
-
-        impl OpCode {
-            pub fn code(&self) -> &'static str {
-                match self {
-                    $(Self::$opname => stringify!($opcode)),*
-                }
+    (
+        $(mod $clmod:ident => $emod:ident => $opmod:ident {
+            $($clname:ident => $ename:ident => ($($oparg:expr),*)$(: $opreturn:expr)? => $opname:ident),*
+        }),*
+    ) => {
+        paste! {
+            pub enum OpCode {
+                // Double recursion so $ename can be unwound as well as $emod
+                $(
+                    $(
+                        [<$emod $ename>]
+                    ),*
+                ),*
             }
 
-            // TODO: maybe make lifetime static?
-            pub fn args(&self) -> &[Type] {
-                match self {
-                    $(Self::$opname => &[$($oparg),*]),*
+            impl OpCode {
+                pub fn code(&self) -> &'static str {
+                    match self {
+                        $(
+                            $(
+                                Self::[<$emod $ename>] => stringify!([<$opmod $opname>])
+                            ),*
+                        ),*
+                    }
                 }
-            }
 
-            pub fn returns(&self) -> Type {
-                match self {
-                    $(Self::$opname => $opreturn),*
+                pub fn args(&self) -> &'static [Type] {
+                    match self {
+                        $(
+                            $(
+                                Self::[<$emod $ename>] => &[$($oparg),*]
+                            ),*
+                        ),*
+                    }
+                }
+
+                pub fn returns(&self) -> Option<Type> {
+                    match self {
+                        $(
+                            $(
+                                Self::[<$emod $ename>] => create_optional!($($opreturn)?)
+                            ),*
+                        ),*
+                    }
                 }
             }
         }
     };
 }
 
-create_opcodes! {
-    // Motion
-    MotionMoveSteps => (Number): Nothing => motion_movesteps,
-    MotionGotoXY => (Number, Number): Nothing => motion_gotoxy,
-    MotionTurnRight => (Number): Nothing => motion_turnright,
-    MotionTurnLeft => (Number): Nothing => motion_turnleft,
-    MotionSetX => (Number): Nothing => motion_setx,
-    MotionSetY => (Number): Nothing => motion_sety,
-    MotionChangeX => (Number): Nothing => motion_changexby,
-    MotionChangeY => (Number): Nothing => motion_changeyby,
-
-    MotionGetX => (): Number => motion_xposition,
-    MotionGetY => (): Number => motion_yposition,
-
-    // Looks
-    LooksSay => (Text): Nothing => looks_say
-}
+create_opcodes!(
+    mod motion => Motion => motion_ {
+        move_steps => MoveSteps => (Number) => movesteps,
+        goto_xy => GotoXY => (Number, Number) => gotoxy,
+        turn_right => TurnRight => (Number) => turnright,
+        turn_left => TurnLeft => (Number) => turnleft,
+        get_x => GetX => (): Number => xposition,
+        get_y => GetY => (): Number => yposition
+    },
+    mod looks => Looks => looks_ {
+        say => Say => (Text) => say,
+        say_for => SayFor => (Text, Number) => sayforsecs
+    }
+);
