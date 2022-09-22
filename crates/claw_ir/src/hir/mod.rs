fe6189a725f6aec6b51795603d9eea7b97b31f28
BD103 <59022059+BD103@users.noreply.github.com>
2022-09-22 09:14:06 -0400

    feat(ir): expr returns function

--- crates/claw_ir/src/hir/mod.rs
+++ crates/claw_ir/src/hir/mod.rs
@@ -1,4 +1,4 @@
-use claw_stdlib::OpCode;
+use claw_stdlib::{OpCode, Type};
 
 pub struct Func {
     pub name: String,
@@ -8,5 +8,19 @@ pub enum Expr {
     Call {
         opcode: OpCode,
         args: Vec<Expr>,
+    },
+    Number(String),
+    Text(String),
+    Boolean(bool),
+}
+
+impl Expr {
+    pub fn returns(&self) -> Option<Type> {
+        match self {
+            Self::Call { opcode, .. } => opcode.returns(),
+            Self::Number(_) => Some(Type::Number),
+            Self::Text(_) => Some(Type::Text),
+            Self::Boolean(_) => Some(Type::Boolean),
+        }
     }
 }
