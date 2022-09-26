097c211d5fc3066228d0a80ea942e6e4248d6766
BD103 <59022059+BD103@users.noreply.github.com>
2022-09-26 13:38:05 -0400

    BREAKING(parse): ok rewrite

--- crates/claw_parse/src/lib.rs
+++ crates/claw_parse/src/lib.rs
@@ -1,25 +1,7 @@
 #![doc = include_str!("../README.md")]
-#![deny(missing_docs)]
-
-pub mod ast;
-mod error;
-pub mod parser;
 
 use ariadne::Report;
-use chumsky::Parser;
 
-pub use crate::{
-    ast::AST,
-    error::{build_report, get_source, ParseError},
-    parser::create_parser,
-};
-
-/// Parses a [`str`] into [`AST`].
-///
-/// # Errors
-///
-/// If `script` is malformed, this will return a [`Report`] from the [`ariadne`] crate.
-pub fn parse(script: &str) -> Result<AST, Box<Report>> {
-    let parser = create_parser();
-    build_report(parser.parse(script))
+pub fn parse(script: &str) -> Result<(), Box<Report>> {
+    Ok(())
 }
