6236f94546f7194209fbf6fc581b8d7693831bc5
BD103 <59022059+BD103@users.noreply.github.com>
2022-10-13 09:47:53 -0400

    feat(parse): create parse error and report generator

--- crates/claw_parse/src/error/mod.rs
+++ crates/claw_parse/src/error/mod.rs
@@ -1,3 +1,12 @@
+use ariadne::{Report, ReportKind};
 use chumsky::error::Simple;
 
+use crate::lex::Token;
+
 pub type LexError = Simple<char>;
+pub type ParseError = Simple<Token>;
+
+pub fn generate_token_report(errors: Vec<Simple<char>>) -> Report {
+    Report::build(ReportKind::Error, (), 0)
+        .finish()
+}
