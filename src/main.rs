92969c4c4a6dea6e542fbd087aab7f5b8d971b1a
BD103 <59022059+BD103@users.noreply.github.com>
2022-11-16 10:49:34 -0500

    feat: set up tracing subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use tracing::Level;
+use tracing_subscriber::FmtSubscriber;
+
+#[cfg(not(debug_assertions))]
+const MAX_LEVEL: Level = Level::WARN;
+
+#[cfg(debug_assertions)]
+const MAX_LEVEL: Level = Level::TRACE;
+
 fn main() {
-    todo!()
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(MAX_LEVEL)
+        .finish();
+    
+
+
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("Error setting global logging subscriber.");
+
+    todo!();
 }
